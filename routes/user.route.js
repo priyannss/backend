1c5f5cd69b19cd0c9a3a432ab1e0eb36a291fbe2
Pankaj <pankaj.mirdha22b@iiitg.ac.in>
2025-03-09 11:05:11 +0530

    Add database

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import { verifyToken } from '../middleware/user.middleware.js';
-import {getLikedSongs,getPlaylists,getRecommendations,getRecentlyPlayed, getUser,getSong,login,register,logout} from '../controllers/user.controller';
+import {getLikedSongs,getPlaylists,getRecommendations,getRecentlyPlayed,
+     getUser,getSong,login,register,logout,getPlaylistSongs} from '../controllers/user.controller';
 const router = Router();
 
 router.post('/getLikedSongs',verifyToken,getLikedSongs)
@@ -10,9 +11,10 @@ router.post('/getRecommendations',verifyToken,getRecommendations)
 router.post('/getRecentlyPlayed',verifyToken,getRecentlyPlayed)
 router.post('/getUser',verifyToken,getUser)
 router.post('getSong',verifyToken,getSong)
+router.post('/getPlaylistSongs',verifyToken,getPlaylistSongs)
+router.post('/logout',verifyToken,logout)
 router.post('/login',login)
 router.post('/register',register)
-router.post('/logout',verifyToken,logout)
 
 export default router
 
